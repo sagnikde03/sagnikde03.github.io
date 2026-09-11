baa618787badedb3d6a2e2063ff429cd70019f67
Sagnik De <133079197+sagnikde03@users.noreply.github.com>
2026-09-11 18:58:27 +0530

    updating font

--- assets/js/custom-enhancements.js
+++ assets/js/custom-enhancements.js
@@ -68,7 +68,7 @@
 
     // Sidebar link list (location, university, email, Scholar, ORCID, GitHub, LinkedIn)
     document.querySelectorAll(".author__urls li, .author__urls a").forEach(function (el) {
-      el.style.fontSize = "0.75rem";
+      el.style.fontSize = "0.6rem";
     });
 
     document.querySelectorAll(".page__content h2").forEach(function (el) {
